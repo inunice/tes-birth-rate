8150a3162b4a763ff6ed52ebf9130b4b16c82cad
inunice <ammanguan@up.edu.ph>
2024-04-21 18:03:17 +0800

    docs: add links to dataset & collection

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -6,6 +6,21 @@ export default async function Overview() {
   return (
     <div className="my-[200px] flex flex-col items-center justify-center gap-10">
       <SectionTitle title="Collection" />
+      <a className="text-teal" href="https://github.com/inunice/tes-birth-rate">
+        View our repository here!
+      </a>
+      <a
+        className="text-teal"
+        href="https://docs.google.com/document/d/1enEdHeMk50b3VRlD1AZu_6Ux9ikRHa7WwSGKE8lVFJM/edit?usp=sharing"
+      >
+        Take a look at our data collection process
+      </a>
+      <a
+        className="text-teal"
+        href="https://docs.google.com/spreadsheets/d/1nwAFaO6RvoAr0iBZu7_pj_rHSNj9LChn3vYykx4Zfc0/edit?usp=sharing"
+      >
+        Check out our dataset!
+      </a>
     </div>
   );
 }
