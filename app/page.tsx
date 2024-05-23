baa213600cd8182276c02c8dd9a855652e108ff5
inunice <ammanguan@up.edu.ph>
2024-05-24 05:52:41 +0800

    feat: add all sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import NavigationBar from "@/components/NavigationBar";
 import Header from "@/components/Header";
 import Overview from "@/components/Overview";
 import Collection from "@/components/Collection";
+import Exploration from "@/components/Exploration";
+import Modeling from "@/components/Modeling";
+import Results from "@/components/Results";
+import Conclusion from "@/components/Conclusion";
 import Team from "@/components/Team";
 
 export default function Home() {
@@ -12,6 +16,10 @@ export default function Home() {
       <Header />
       <Overview />
       <Collection />
+      <Exploration />
+      <Modeling />
+      <Results />
+      <Conclusion />
       <Team />
     </div>
   );
---
+++ components/Exploration.tsx
@@ -0,0 +1,10 @@
+import "@/app/globals.css";
+import SectionTitle from "@/components/ui/SectionTitle";
+
+export default async function Exploration() {
+  return (
+    <div className="my-[200px] flex flex-col items-center justify-center gap-10">
+      <SectionTitle title="Exploration" />
+    </div>
+  );
+}
