824e06e2f1aa9bbad64a772d641ea592602d583e
Jose <joseernestotmanan@gmail.com>
2024-05-23 22:59:51 +0800

    feat(team): added card for Jose

--- components/Team.tsx
+++ components/Team.tsx
@@ -42,7 +42,27 @@ export default async function Team() {
         </div>
         <div className="border p-8 rounded-lg bg-naples-yellow  w-1/3">
           <h2 className="text-xl font-bold font-urbanist">Jose Tomanan</h2>
-          <p className="mt-2">This is the third card.</p>
+          <p className="mt-3 text-md">
+            Heya! I'm Jose, a second year Computer Science student from UP Diliman. I am interested in project management and software development.
+          </p>
+          <p className="mt-3">
+            I spend most of my time away from the keyboard, consuming non-tech related passions such as fashion, fitness, and basketball. Though more recently, I have been taking active interest in data science, figuring things out on my own and visualizing results. I look forward to learning more about tech as I progress in my stint in this course.
+          </p>
+          <div>
+            <div className="flex flex-row gap-2 items-center mt-6">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="white"
+                  className="w-4 h-4"
+                >
+                  <path d="M19.5 22.5a3 3 0 0 0 3-3v-8.174l-6.879 4.022 3.485 1.876a.75.75 0 1 1-.712 1.321l-5.683-3.06a1.5 1.5 0 0 0-1.422 0l-5.683 3.06a.75.75 0 0 1-.712-1.32l3.485-1.877L1.5 11.326V19.5a3 3 0 0 0 3 3h15Z" />
+                  <path d="M1.5 9.589v-.745a3 3 0 0 1 1.578-2.642l7.5-4.038a3 3 0 0 1 2.844 0l7.5 4.038A3 3 0 0 1 22.5 8.844v.745l-8.426 4.926-.652-.351a3 3 0 0 0-2.844 0l-.652.351L1.5 9.589Z" />
+                </svg>
+                {/* COPY PASTED FROM AYEN; NOT SURE WHATS DIFFERENT */}
+                <span>jdtomanan@up.edu.ph</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
