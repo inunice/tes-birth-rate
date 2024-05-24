e82eaacf788348c2492f439efb0d1aea7a1fb6f9
inunice <ammanguan@up.edu.ph>
2024-05-24 11:13:51 +0800

    docs: add link of python notebook

--- app/Exploration.tsx
+++ app/Exploration.tsx
@@ -1,10 +1,16 @@
 import "@/app/globals.css";
 import SectionTitle from "@/components/ui/SectionTitle";
+import Button from "@/components/ui/Button";
 
 export default async function Exploration() {
   return (
-    <div className="flex flex-col items-center justify-center gap-10">
+    <div className="w-7/12 flex flex-col items-center justify-center gap-10">
       <SectionTitle title="Exploration" />
+      <Button
+        className="ml-auto"
+        name="Take a look at our full exploration tour!"
+        link="https://colab.research.google.com/drive/1yBrVKPBKlfGQR4bbGP-N8Q9ZNd9hW0yD?usp=sharing"
+      />
     </div>
   );
 }
